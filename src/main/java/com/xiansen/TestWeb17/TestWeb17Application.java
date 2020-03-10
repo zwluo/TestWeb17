050119db09584c62e462b7d494597817dc5ed674
zwluo <luozhiweideyou@163.com>
2020-03-10 11:39:30 +0800

    lzw N001 项目提交

---
+++ src/main/java/com/xiansen/TestWeb17/TestWeb17Application.java
@@ -0,0 +1,13 @@
+package com.xiansen.TestWeb17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWeb17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestWeb17Application.class, args);
+	}
+
+}
